2b55b5fe1f6a1cb0dd1b7c80ade33890be964c52
Brandon Zimmerman <bzimmermandev@icloud.com>
2019-04-13 19:38:37 -0500

    Renamed module Tests to Osrs.Skills.Tests; added test; reorganized tests

--- tests/Osrs/Skills/Tests.elm
+++ tests/Osrs/Skills/Tests.elm
@@ -1,9 +1,45 @@
-module Tests exposing (suite)
+module Osrs.Skills.Tests exposing (suite)
 
+import Dict
 import Expect exposing (Expectation, FloatingPointTolerance(..))
 import Fuzz exposing (Fuzzer, int, list, string)
 import Osrs.Skills as Skills
 import Test exposing (..)
+
+
+
+{- Mockup data -}
+
+
+mockTable =
+    Skills.emptyTable
+        |> Skills.updateExperience "Attack" 743664
+        |> Skills.updateExperience "Defence" 712396
+        |> Skills.updateExperience "Strength" 1560454
+        |> Skills.updateExperience "Hitpoints" 1251121
+        |> Skills.updateExperience "Ranged" 800212
+        |> Skills.updateExperience "Prayer" 334994
+        |> Skills.updateExperience "Magic" 653790
+        |> Skills.updateExperience "Cooking" 824650
+        |> Skills.updateExperience "Woodcutting" 1177033
+        |> Skills.updateExperience "Fletching" 782815
+        |> Skills.updateExperience "Fishing" 625917
+        |> Skills.updateExperience "Firemaking" 834561
+        |> Skills.updateExperience "Crafting" 657204
+        |> Skills.updateExperience "Smithing" 203301
+        |> Skills.updateExperience "Mining" 434723
+        |> Skills.updateExperience "Herblore" 201645
+        |> Skills.updateExperience "Agility" 751122
+        |> Skills.updateExperience "Thieving" 339774
+        |> Skills.updateExperience "Slayer" 552844
+        |> Skills.updateExperience "Farming" 603234
+        |> Skills.updateExperience "Runecraft" 213745
+        |> Skills.updateExperience "Hunter" 248541
+        |> Skills.updateExperience "Construction" 129927
+
+
+
+{- Test suite -}
 
 
 suite : Test
@@ -14,13 +50,19 @@ suite =
                 Skills.emptyTable
                     |> Skills.experienceList
                     |> Expect.equal (List.repeat (List.length Skills.names) 0)
-        , test "updateExperience |> experience returns the correct xp in the updated table" <|
+        , test "updateExperience |> getExperience returns the correct XP in the updated table" <|
             \_ ->
                 Skills.emptyTable
                     |> Skills.updateExperience "Attack" 5000
                     |> Skills.getExperience "Attack"
                     |> Expect.equal 5000
-        , test "experienceAtLevel returns the correct xp for known levels" <|
+        , test "toDict returns a properly-sized dict" <|
+            \_ ->
+                Skills.emptyTable
+                    |> Skills.toDict
+                    |> Dict.size
+                    |> Expect.equal (List.length Skills.names)
+        , test "experienceAtLevel returns the correct XP for known levels" <|
             \_ ->
                 Expect.all
                     [ \_ ->
@@ -61,9 +103,6 @@ suite =
                         Skills.level 1
                             |> Expect.equal 1
                     , \_ ->
-                        Skills.level 188884740
-                            |> Expect.equal 126
-                    , \_ ->
                         Skills.level 200000000
                             |> Expect.equal 126
                     , \_ ->
@@ -71,34 +110,11 @@ suite =
                             |> Expect.equal 126
                     ]
                     ()
-        , let
-            mockTable =
-                Skills.emptyTable
-                    |> Skills.updateExperience "Attack" 743664
-                    |> Skills.updateExperience "Defence" 712396
-                    |> Skills.updateExperience "Strength" 1560454
-                    |> Skills.updateExperience "Hitpoints" 1251121
-                    |> Skills.updateExperience "Ranged" 800212
-                    |> Skills.updateExperience "Prayer" 334994
-                    |> Skills.updateExperience "Magic" 653790
-                    |> Skills.updateExperience "Cooking" 824650
-                    |> Skills.updateExperience "Woodcutting" 1177033
-                    |> Skills.updateExperience "Fletching" 782815
-                    |> Skills.updateExperience "Fishing" 625917
-                    |> Skills.updateExperience "Firemaking" 834561
-                    |> Skills.updateExperience "Crafting" 657204
-                    |> Skills.updateExperience "Smithing" 203301
-                    |> Skills.updateExperience "Mining" 434723
-                    |> Skills.updateExperience "Herblore" 201645
-                    |> Skills.updateExperience "Agility" 751122
-                    |> Skills.updateExperience "Thieving" 339774
-                    |> Skills.updateExperience "Slayer" 552844
-                    |> Skills.updateExperience "Farming" 603234
-                    |> Skills.updateExperience "Runecraft" 213745
-                    |> Skills.updateExperience "Hunter" 248541
-                    |> Skills.updateExperience "Construction" 129927
-          in
-          describe "Tests using a known `Table`"
+        , test "remainingExperience returns XP-til-200m above virtual level 126" <|
+            \_ ->
+                Skills.remainingExperience 199999999
+                    |> Expect.equal 1
+        , describe "Tests using a known table"
             [ test "totalExperience returns the expected result for a known table" <|
                 \_ ->
                     mockTable
@@ -124,10 +140,6 @@ suite =
                                 |> Expect.equal 21165
                         ]
                         ()
-            , test "remainingExperience returns the xp til 200m above virtual level 126" <|
-                \_ ->
-                    Skills.remainingExperience 199999999
-                        |> Expect.equal 1
             , test "level returns the correct levels for skills in a known table" <|
                 \_ ->
                     Expect.all
